bf27508c0818a6118d4ccb6d2238235cff756e42
n1ico <125403659+n1ico@users.noreply.github.com>
2024-03-29 20:16:20 -0300

    Update index.php

--- calculadora/index.php
+++ calculadora/index.php
@@ -1,39 +1,57 @@
+<?php
+    $x = 0;
+    $y = 0;
+    $resultado = 0;
+    $calcular = 'adicao';
+    if(isset($_GET['x'], $_GET['y'], $_GET['calcular'])){
+        $num1 = $_GET['x'];
+        $num2 = $_GET['y'];
+        $calcular = $_GET['calcular'];
+
+        switch($calcular){
+            case 'adicao':
+                $resultado = $x + $y;
+                break;
+            case 'subtracao':
+                $resultado = $x - $y;
+                break;
+            case 'multiplicacao':
+                $resultado = $x * $y;
+                break;
+            case 'divisao':
+                $resultado = $x / $y;
+                break;
+        }
+    }
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calculadora</title>
-    <link rel="stylesheet" href="estilo.css">
+    <title>Calculadora simples</title>
 </head>
 <body>
- <div class="container">
-    <div id="contorno">
-       <div class="conteiner">
-<?php 
-
- if (isset($_GET['operacao']) && isset($_GET['x']) && isset($_GET['y'])){
-
- if($_GET['operacao']== "adicao"){
-     echo $_GET['x'] + $_GET['y'];
-}
-
- else if($_GET['operacao'] == "subtracao"){
-    echo $_GET['x'] - $_GET['y'];
-}
-
- else if($_GET['operacao']== "multiplicacao"){
-    echo $_GET['x'] * $_GET['y'];
-}
-
- else if($_GET['operacao']== "divisao"){
-    echo $_GET['x'] / $_GET['y'];
-}}
-
- else echo "<h1>Nenhuma das opções foi selecionada.</h1>"
- 
-?>
-</div>
-</div>
+    <form>
+        <h3> Calculadora simples: </h3>
+        <br><br>
+        Valor um :
+        <br>
+        <input type="number" name="x"><br>
+        Valor dois :
+        <br>
+        <input type="number" name="y">
+        <br><br>
+        <select name="calcular">
+            <option value="adiacao">Adição</option>
+            <option value="subtracao">Subtração</option>
+            <option value="multiplicacao">Multiplicação</option>
+            <option value="divisao">Dividisão</option>
+        </select>
+        <input type="submit" value="calcular">
+        <br><br>
+        <p>O resultado é <?= $resultado ?></p>
+    </form>
 </body>
 </html>
